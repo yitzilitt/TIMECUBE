f97235e8241d60099d11cdfd3c73f3f070177ccf
yitzilitt <yitzilitt@gmail.com>
2023-05-25 04:48:41 -0400

    attempting to fix a bug that broke everything

--- api/upload.js
+++ api/upload.js
@@ -1,25 +1,39 @@
-import multer from 'multer';
-import { readFile, writeFile } from 'fs';
+const { send } = require('micro');
+const multer  = require('multer');
+const fs = require('fs');
 const upload = multer({ dest: '/tmp/' });
 
-module.exports = upload.single('filename'), function (req, res) {
-  console.log(req.file);
+const uploadMiddleware = upload.single('filename');
 
-  // Read file
-  readFile(req.file.path, 'utf8', function(err, data) {
-    if (err) return res.status(500).send(err);
+module.exports = async (req, res) => {
+  uploadMiddleware(req, res, function(err) {
+    if (err instanceof multer.MulterError) {
+      send(res, 500, err);
+      return;
+    } else if (err) {
+      send(res, 500, err);
+      return;
+    }
 
-    // Perform operation on file
-    const newData = data + '\nAppend some text here!';
+    // Now multer has processed the request, you can access your form data
+    console.log(req.file);
 
-    // Write operation result to new file
-    const outputPath = `/tmp/edited_${req.file.originalname}`;
-    writeFile(outputPath, newData, function(err) {
-      if (err) return res.status(500).send(err);
-      console.log('File edited successfully!');
+    // Read file
+    fs.readFile(req.file.path, 'utf8', function(err, data) {
+      if (err) return send(res, 500, err);
 
-      // Send file download response
-      res.download(outputPath);
+      // Perform operation on file
+      const newData = data + '\nAppend some text here!';
+
+      // Write operation result to new file
+      const outputPath = `/tmp/edited_${req.file.originalname}`;
+      fs.writeFile(outputPath, newData, function(err) {
+        if (err) return send(res, 500, err);
+        console.log('File edited successfully!');
+
+        // Send file download response
+        res.download(outputPath);
+      });
     });
   });
-}
+};
